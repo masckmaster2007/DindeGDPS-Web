283478f3fb53b61a1030910182cca351b65a06c9
masckmaster2007 <63162857+masckmaster2007@users.noreply.github.com>
2022-06-13 12:20:12 +0200

    Update index.js

--- index.js
+++ index.js
@@ -72,7 +72,7 @@ app.use(async function(req, res, next) {
   if (!subdomains.length) subdomains = [""]
   req.server = app.servers.find(x => subdomains.includes(x.id.toLowerCase()))
   //if (subdomains.length > 1 || !req.server) return res.redirect("http://" + "dgdps.herokuapp.com" + req.originalUrl)
-  if (subdomains.length > 1 || !req.server) return res.redirect("http://" + req.get('host').join(".") + req.originalUrl)
+   if (subdomains.length > 1 || !req.server) return res.redirect("http://" + req.get('host').split(".").join(".") + req.originalUrl)
   // will expand this in the future :wink:
   res.sendError = function(errorCode=500) {
     res.status(errorCode).send("-1")
